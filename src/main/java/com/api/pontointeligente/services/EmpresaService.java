2d0cfd45a9a2f39c0694e37719685715f04d17a9
Wallace C. Ferrante <wallace.ferrante@monitoratec.com.br>
2021-02-24 16:25:36 -0300

    Adicionado o serviço e implementação para Empresa

---
+++ src/main/java/com/api/pontointeligente/services/EmpresaService.java
@@ -0,0 +1,24 @@
+package com.api.pontointeligente.services;
+
+import java.util.Optional;
+
+import com.api.pontointeligente.entities.Empresa;
+
+public interface EmpresaService {
+    
+    /**
+     * Retorna uma empresa dado um CNPJ
+     * 
+     * @param cnpj
+     * @return Optional<Empresa>
+     */
+    Optional<Empresa> buscarPorCnpj(String cnpj);
+
+    /**
+     * Cadastra uma nova empresa na base de dados.
+     * 
+     * @param empresa
+     * @return Empresa
+     */
+    Empresa persistir(Empresa empresa);
+}
